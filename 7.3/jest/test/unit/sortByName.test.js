3906ce28c5d63b9d810cbef161ca0f8ac828ae36
Konstantin Voytenko <koc.voytenko@gmail.com>
2022-12-12 10:23:20 +0300

    First Task Completed

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -15,3 +15,11 @@ describe("Books names test suit", () => {
     ]);
   });
 });
+
+describe("New books names test suit", () => {
+  it("Books names should not be sorted", () => {
+    expect(
+      sorting.sortByName(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"])
+    ).toEqual(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"]);
+  });
+});
