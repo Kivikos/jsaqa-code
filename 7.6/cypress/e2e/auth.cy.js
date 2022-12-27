be9c801d647399539facdb578f0c7c951f0b4da2
Kivikos <koc.voytenko@gmail.com>
2022-12-27 10:21:05 +0300

    Some fixes

--- 7.6/cypress/e2e/auth.cy.js
+++ 7.6/cypress/e2e/auth.cy.js
@@ -26,29 +26,29 @@ it.skip("Should not login with empty password", () => {
 });
 
 beforeEach(() => {
-  cy.visit("/");
+  cy.visit("http://localhost:3000/");
   cy.login("test@test.com", "test");
   cy.contains("Добро пожаловать").should("be.visible");
 });
 describe("When user add the books", () => {
   it("Should add a book", () => {
     cy.get(".p-0 > .btn").click();
-    cy.get("#title").type("Шерлок Холмс");
+    cy.get("#title").type("Портрет Дориана Грея");
     cy.get("#description").type("Детектив");
     cy.get("#authors").type("Оскар Уайлд");
     cy.contains("Submit").click();
-    cy.contains("Шерлок Холмс").should("be.visible");
+    cy.contains("Портрет Дориана Грея").should("be.visible");
   });
   it("Should add a book to favorites", () => {
-    cy.contains("Война миров").contains("Add to favorite").click();
+    cy.contains("Портрет Дориана Грея").contains("Add to favorite").click();
     cy.contains("Favorites").click();
-    cy.contains("Шерлок Холмс").should("be.visible");
+    cy.contains("Портрет Дориана Грея").should("be.visible");
   });
 
   it("Should delete a book from favorites", () => {
     cy.contains("Books list").click();
     cy.contains("Favorites").click();
-    cy.contains("Шерлок Холмс").contains("Delete from favorite").click();
-    cy.contains("Шерлок Холмс").should("not.exist");
+    cy.contains("Портрет Дориана Грея").contains("Delete from favorite").click();
+    cy.contains("Портрет Дориана Грея").should("not.exist");
   });
 });
